765ef2fe9ea4cc731f93464048a58e9e8c2bad9b
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-05-11 17:19:00 -0300

    Adicionados métodos de validação para os atributos do paciente.

--- ControleDeMedicamentos.ConsoleApp/ModuloPacientes/Paciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPacientes/Paciente.cs
@@ -24,7 +24,131 @@ public class Paciente : EntidadeBase
 
   public override List<string> Validar()
   {
-    throw new NotImplementedException();
+    List<string> erros = [];
+
+    ValidarNome(erros);
+    ValidarTelefone(erros);
+    ValidarCPF(erros);
+    ValidarCartaoSUS(erros);
+
+    return erros;
+  }
+
+  private void ValidarNome(List<string> erros)
+  {
+    if (Nome.Length < 3 || Nome.Length > 100)
+      erros.Add("O campo \"Nome\" deve conter entre 3 e 100 caracteres.");
+  }
+
+  private void ValidarTelefone(List<string> erros)
+  {
+    string telefoneEncurtado = RemoverFormatacao(Telefone);
+
+    if (telefoneEncurtado.StartsWith("0"))
+      telefoneEncurtado = telefoneEncurtado.Substring(1);
+
+    bool telefoneValido = true;
+
+    if (telefoneEncurtado.Length < 10 || telefoneEncurtado.Length > 11)
+    {
+      erros.Add("O campo \"Telefone\" deve conter entre 10 e 11 dígitos.");
+      telefoneValido = false;
+    }
+
+    if (!ContemSomenteDigitos(telefoneEncurtado))
+    {
+      erros.Add("O campo \"Telefone\" deve conter apenas dígitos.");
+      telefoneValido = false;
+    }
+
+    if (telefoneValido)
+    {
+      if (telefoneEncurtado.Length == 10)
+      {
+        Telefone = Convert.ToUInt64(telefoneEncurtado)
+            .ToString(@"\(00\) 0000\-0000");
+      }
+      else
+      {
+        Telefone = Convert.ToUInt64(telefoneEncurtado)
+            .ToString(@"\(00\) 00000\-0000");
+      }
+    }
+  }
+
+  private void ValidarCPF(List<string> erros)
+  {
+    if (string.IsNullOrWhiteSpace(CPF))
+    {
+      erros.Add("O campo \"CPF\" deve ser preenchido.");
+      return;
+    }
+
+    string cpfEncurtado = RemoverFormatacao(CPF);
+
+    bool cpfValido = true;
+
+    if (cpfEncurtado.Length != 11)
+    {
+      erros.Add("O campo \"CPF\" deve conter 11 dígitos.");
+      cpfValido = false;
+    }
+
+    if (!ContemSomenteDigitos(cpfEncurtado))
+    {
+      erros.Add("O campo \"CPF\" deve conter somente dígitos.");
+      cpfValido = false;
+    }
+
+    if (cpfValido)
+    {
+      CPF = Convert.ToUInt64(cpfEncurtado)
+          .ToString(@"000\.000\.000\-00");
+    }
+  }
+
+  private void ValidarCartaoSUS(List<string> erros)
+  {
+    string cartaoSUSFormatado = RemoverFormatacao(CartaoSUS);
+
+    bool cartaoValido = true;
+
+    if (cartaoSUSFormatado.Length != 15)
+    {
+      erros.Add("O campo \"Cartão SUS\" deve conter 15 dígitos.");
+      cartaoValido = false;
+    }
+
+    if (!ContemSomenteDigitos(cartaoSUSFormatado))
+    {
+      erros.Add("O campo \"Cartão SUS\" deve conter apenas dígitos.");
+      cartaoValido = false;
+    }
+
+    if (cartaoValido)
+      CartaoSUS = cartaoSUSFormatado;
+  }
+
+  private bool ContemSomenteDigitos(string valor)
+  {
+    for (int i = 0; i < valor.Length; i++)
+    {
+      if (!char.IsDigit(valor[i]))
+        return false;
+    }
+
+    return true;
+  }
+
+  private string RemoverFormatacao(string valor)
+  {
+    return valor
+        .Replace(" ", "")
+        .Replace("-", "")
+        .Replace(".", "")
+        .Replace("/", "")
+        .Replace("(", "")
+        .Replace(")", "");
   }
 
   public override void AtualizarDados(EntidadeBase entidadeAtualizada)
