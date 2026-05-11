08f6c9255e2096888240c227ae100bc16e048083
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-05-11 17:23:11 -0300

    Adicionado método ObterDadosCadastrais na TelaPaciente

--- ControleDeMedicamentos.ConsoleApp/ModuloPacientes/TelaPaciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPacientes/TelaPaciente.cs
@@ -15,6 +15,18 @@ public class TelaPaciente : TelaBase<Paciente>, ITelaCrud, ITelaOpcoes
 
   protected override Paciente ObterDadosCadastrais()
   {
-    throw new NotImplementedException();
+    Console.Write("Digite o nome do paciente: ");
+    string nome = Console.ReadLine() ?? string.Empty;
+
+    Console.Write("Digite o telefone do paciente: ");
+    string telefone = Console.ReadLine() ?? string.Empty;
+
+    Console.Write("Digite o número do cartão do SUS: ");
+    string cartaoSUS = Console.ReadLine() ?? string.Empty;
+
+    Console.Write("Digite o CPF do paciente: ");
+    string cpf = Console.ReadLine() ?? string.Empty;
+
+    return new Paciente(nome, telefone, cartaoSUS, cpf);
   }
 }
