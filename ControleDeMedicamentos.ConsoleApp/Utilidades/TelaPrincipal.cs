3dd5aa8f01780b5ca557fb3ff8b8035e3a9a0f3c
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-05-11 16:16:13 -0300

    Configurada program e tela principal para fornecedores

--- ControleDeMedicamentos.ConsoleApp/Utilidades/TelaPrincipal.cs
+++ ControleDeMedicamentos.ConsoleApp/Utilidades/TelaPrincipal.cs
@@ -1,12 +1,14 @@
 using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.ModuloFornecedores;
 
 namespace ControleDeMedicamentos.ConsoleApp.Utilidades;
 
 public class TelaPrincipal
 {
-
-  public TelaPrincipal()
+  private readonly IRepositorio<Fornecedor> repositorioFornecedor;
+  public TelaPrincipal(IRepositorio<Fornecedor> repositorioFornecedor)
   {
+    this.repositorioFornecedor = repositorioFornecedor;
 
   }
 
@@ -16,11 +18,15 @@ public class TelaPrincipal
     Console.WriteLine("---------------------------------");
     Console.WriteLine("Controle de Medicamentos");
     Console.WriteLine("---------------------------------");
+    Console.WriteLine("1 - Gestão de Fornecedores");
     Console.WriteLine("S - Sair");
     Console.WriteLine("---------------------------------");
     Console.Write("> ");
     string? opcaoMenuPrincipal = Console.ReadLine()?.ToUpper();
 
+    if (opcaoMenuPrincipal == "1")
+      return new TelaFornecedor(repositorioFornecedor);
+      
     return null;
   }
 }
