bba924eab23f502e4a107ff2c1cdd7a685e8b057
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-05-11 15:52:25 -0300

    Adicionada validação de atributos do fornecedor

--- ControleDeMedicamentos.ConsoleApp/ModuloFornecedores/Fornecedor.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloFornecedores/Fornecedor.cs
@@ -19,7 +19,89 @@ public class Fornecedor : EntidadeBase
 
     public override List<string> Validar()
     {
-        throw new NotImplementedException();
+        List<string> erros = new List<string>();
+
+        if (Nome.Length < 3 || Nome.Length > 100)
+            erros.Add("O campo \"Nome\" deve conter entre 3 e 100 caracteres.");
+
+        int contadorDigitos = 0;
+        bool contemLetraOuSimbolo = false;
+
+        string telefoneEncurtado = Telefone.Replace(" ", "").Replace("-", "");
+
+        if (telefoneEncurtado.StartsWith("0"))
+            telefoneEncurtado = telefoneEncurtado.Substring(1);
+
+        for (int i = 0; i < telefoneEncurtado.Length; i++)
+        {
+            char caractereAtual = telefoneEncurtado[i];
+
+            if (char.IsDigit(caractereAtual))
+            {
+                contadorDigitos++;
+            }
+            else
+            {
+                contemLetraOuSimbolo = true;
+                break;
+            }
+        }
+
+        if (contadorDigitos < 10 || contadorDigitos > 11)
+            erros.Add("O campo \"Telefone\" deve conter entre 10 e 11 dígitos");
+
+        if (contemLetraOuSimbolo)
+            erros.Add("O campo \"Telefone\" deve conter apenas dígitos");
+
+        if (erros.Count == 0)
+        {
+            if (contadorDigitos == 10)
+            {
+                Telefone = Convert.ToUInt64(telefoneEncurtado)
+                            .ToString(@"\(00\) 0000\-0000");
+            }
+            else if (contadorDigitos == 11)
+            {
+                Telefone = Convert.ToUInt64(telefoneEncurtado)
+                            .ToString(@"\(00\) 00000\-0000");
+            }
+        }
+
+        if (string.IsNullOrWhiteSpace(Cnpj))
+            erros.Add("O campo \"CNPJ\" deve ser preenchido");
+
+        int contadorDigitosCnpj = 0;
+        bool contemLetraOuSimboloCnpj = false;
+
+        string cnpjEncurtado = Cnpj.Replace(" ", "").Replace("-", "").Replace(".", "").Replace("/", "");
+
+        for (int i = 0; i < cnpjEncurtado.Length; i++)
+        {
+            char caractereAtualCnpj = cnpjEncurtado[i];
+
+            if (char.IsDigit(caractereAtualCnpj))
+            {
+                contadorDigitosCnpj++;
+            }
+            else
+            {
+                contemLetraOuSimboloCnpj = true;
+                break;
+            }
+        }
+
+        if (contadorDigitosCnpj != 14)
+            erros.Add("O campo \"CNPJ\" deve conter 14 dígitos");
+
+        if (contemLetraOuSimboloCnpj)
+            erros.Add("O campo \"CNPJ\" deve conter somente dígitos");
+
+        if (erros.Count == 0)
+        {
+            Cnpj = Convert.ToUInt64(cnpjEncurtado).ToString(@"00\.000\.000\/0000\-00");
+        }
+
+        return erros;
     }
 
     public override void AtualizarDados(EntidadeBase entidadeAtualizada)
