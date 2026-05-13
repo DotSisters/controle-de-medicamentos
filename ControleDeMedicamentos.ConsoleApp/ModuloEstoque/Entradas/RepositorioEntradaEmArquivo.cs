f8b3acef8f441530d07e21d977f18dcc977c39cd
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-05-13 10:23:47 -0300

    Adicionado repositório em memória e em arquivo para entradas de estoque.

---
+++ ControleDeMedicamentos.ConsoleApp/ModuloEstoque/Entradas/RepositorioEntradaEmArquivo.cs
@@ -0,0 +1,17 @@
+using System;
+using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+using ControleDeMedicamentos.ConsoleApp.Compartilhado.Arquivos;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloEstoque.Entradas;
+
+public class RepositorioEntradaEmArquivo : RepositorioBaseEmArquivo<Entrada>, IRepositorio<Entrada>
+{
+  public RepositorioEntradaEmArquivo(ContextoJson contexto) : base(contexto)
+  {
+  }
+
+  protected override List<Entrada> CarregarRegistros()
+  {
+    return contexto.Entradas;
+  }
+}
---
+++ ControleDeMedicamentos.ConsoleApp/ModuloEstoque/Entradas/RepositorioEntradaEmMemoria.cs
@@ -0,0 +1,5 @@
+using ControleDeMedicamentos.ConsoleApp.Compartilhado.Memoria;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloEstoque.Entradas;
+
+public class RepositorioEntradaEmMemoria : RepositorioBaseEmMemoria<Entrada>;
