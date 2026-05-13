296d49024f039e5199d997f3780e5f39b43bbda8
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-05-13 10:21:53 -0300

    Adicionados métodos de validação e atualização de dados na classe Entrada.

--- ControleDeMedicamentos.ConsoleApp/ModuloEstoque/Entradas/Entrada.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloEstoque/Entradas/Entrada.cs
@@ -4,7 +4,7 @@ using ControleDeMedicamentos.ConsoleApp.ModuloMedicamentos;
 
 namespace ControleDeMedicamentos.ConsoleApp.ModuloEstoque.Entradas;
 
-public class Entrada
+public class Entrada : EntidadeBase
 {
   public DateTime Data { get; set; }
   public Medicamento Medicamento { get; set; }
@@ -23,5 +23,33 @@ public class Entrada
     Quantidade = quantidade;
 
     // Medicamento.RegistrarRequisicaoEntrada(this);
+  }
+
+  public override void AtualizarDados(EntidadeBase entidadeAtualizada)
+  {
+    Entrada entradaAtualizada = (Entrada)entidadeAtualizada;
+
+    Medicamento = entradaAtualizada.Medicamento;
+    Funcionario = entradaAtualizada.Funcionario;
+    Quantidade = entradaAtualizada.Quantidade;
+  }
+
+  public override List<string> Validar()
+  {
+    List<string> erros = new List<string>();
+
+    if (Data == default || Data > DateTime.Now)
+      erros.Add("O campo \"Data\" deve ser uma data válida e não pode ser uma data futura.");
+
+    if (Medicamento == null)
+      erros.Add("O campo \"Medicamento\" é obrigatório.");
+
+    if (Funcionario == null)
+      erros.Add("O campo \"Funcionário\" é obrigatório.");
+
+    // if (Quantidade <= 0)
+    //   erros.Add("O campo 'Quantidade' deve ser um número positivo.");
+
+    return erros;
   }
 }
